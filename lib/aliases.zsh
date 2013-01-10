a92ec949f4d3c8fa723db9f55320c8db72d25b43
jmuraski <muraskijj@hotmail.com>
2013-01-09 22:02:49 -0600

    :q

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -32,7 +32,7 @@ alias editprofile="subl ~/.zshrc"
 alias editalias="subl ~/.oh-my-zsh/lib/aliases.zsh"
 alias editfunctions="subl ~/.oh-my-zsh/lib/functions.zsh"
 alias editdirs="subl ~/.oh-my-zsh/lib/directories.zsh"
-alias cconnectUbuntu="ssh jmuraski@172.16.73.135"
+alias connectUbuntu="ssh jmuraski@172.16.73.135"
 
 alias startpostgres='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
 alias stoppostgres='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
