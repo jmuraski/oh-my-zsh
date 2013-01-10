201b9c3baaa84e4aa5d35d6eb6ab88dae5421ea5
jmuraski <muraskijj@hotmail.com>
2013-01-09 21:18:38 -0600

    added connection to ubuntu server and added scripts for growly notes

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -32,9 +32,11 @@ alias editprofile="subl ~/.zshrc"
 alias editalias="subl ~/.oh-my-zsh/lib/aliases.zsh"
 alias editfunctions="subl ~/.oh-my-zsh/lib/functions.zsh"
 alias editdirs="subl ~/.oh-my-zsh/lib/directories.zsh"
+alias cconnectUbuntu="ssh jmuraski@172.16.73.135"
 
 alias startpostgres='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
 alias stoppostgres='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
+
 
 alias startmongo='/usr/local/Cellar/mongodb/2.2.1-x86_64/mongod run --rest'
 # export CC=gcc
--- lib/directories.zsh
+++ lib/directories.zsh
@@ -24,11 +24,34 @@ export ENV="dev"
 export MYSQL_HOME="/usr/local/mysql"
 export WORKSPACE="$HOME/Documents/workspace"
 export MY_SCRIPTS="$HOME/.oh-my-zsh/scripts"
+export HOMEBREW_HOME="/usr/local/Cellar"
+export RIAK_HOME="$HOMEBREW_HOME/riak/1.2.1-x86_64"
+export HBASE_HOME="$HOMEBREW_HOME/hbase/0.94.2"
+
+alias startriak="$RIAK_HOME/bin/riak start"
+alias stopriak="$RIAK_HOME/bin/riak stop"
+alias starthbase="$HBASE_HOME/bin/start-hbase.sh"
+alias starthbase="$HBASE_HOME/bin/stop-hbase.sh"
 
 alias llw="ll $WORKSPACE"
 alias cds="cd $MY_SCRIPTS"
 alias lls="ll -alF $MY_SCRIPTS"
 alias sshKeys="cd ~/Documents/keys/ssh"
+
+notebook(){
+  cd ~/Documents/Notebooks;
+  if [[ "$1" == "save" ]]; then
+    cd ~/Documents/Notebooks
+    git add .
+    git commit -m 'updating'
+    git push   
+  elif [[ "$1" == "load" ]]; then
+    cd ~/Documents/Notebooks
+    git pull
+  else
+    cd ~/Documents/Notebooks
+  fi
+}
 
 cd () {
   if   [[ "x$*" == "x..." ]]; then
