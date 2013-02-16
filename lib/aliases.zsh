8af2b004b1702ec9413068fa595b74bd20221632
Joseph Muraski <jmuraski@bestbuy.com>
2013-02-16 13:12:35 -0600

    added aliases for amazon servers

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -41,3 +41,6 @@ alias startmongo='/usr/local/Cellar/mongodb/2.2.1-x86_64/mongod run --rest'
 
 alias start1='~/Documents/Virtual\ Machines.localized/startUbuntu.sh'
 alias con1="ssh jmuraski@192.168.163.100"
+alias ssh-ca-dev1='ssh -v -i ~/.ssh/ca-ssh-keypair.pem ec2-user@ec2-54-245-110-30.us-west-2.compute.amazonaws.com'
+alias ssh-ca-dev2='ssh -v -i ~/.ssh/ca-ssh-keypair.pem ec2-user@ec2-54-245-110-32.us-west-2.compute.amazonaws.com'
+alias ssh-jenkins='ssh -v -i ~/.ssh/jenkins-ssh-keypair.pem ec2-user@54.245.238.194'
