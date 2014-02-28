5efe9652c8baf1e1802e1f1cd311c28ba294870b
jmuraski <muraskijj@hotmail.com>
2014-02-28 09:14:32 -0600

    modiged aliases and grails version as well as zshrc

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -59,28 +59,38 @@ alias tailTomcat='tail -f /usr/local/apache-tomcat-7.0.37/logs/catalina.out'
 alias startmongo='/usr/local/Cellar/mongodb/2.4.9/mongod run --rest'
 # export CC=gcc
 
+# Manta Aliases
 alias start1='~/Documents/Virtual\ Machines.localized/startUbuntu.sh'
 alias con1="ssh jmuraski@192.168.163.100"
-alias ssh-ca-dev30='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-245-110-30.us-west-2.compute.amazonaws.com'
-alias ssh-ca-dev-tomcat='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-245-110-30.us-west-2.compute.amazonaws.com'
-alias ssh-ca-dev32='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-245-110-32.us-west-2.compute.amazonaws.com'
-alias ssh-ca-dev-apache='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-245-110-32.us-west-2.compute.amazonaws.com'
-alias ssh-ca-uat40='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-214-28-40.us-west-2.compute.amazonaws.com'
-alias ssh-ca-uat-tomcat='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-214-28-40.us-west-2.compute.amazonaws.com'
-alias ssh-ca-uat35='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-214-28-35.us-west-2.compute.amazonaws.com'
-alias ssh-ca-uat-apache='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-214-28-35.us-west-2.compute.amazonaws.com'
-alias ssh-jenkins='ssh -v -i ~/.ssh/keys/jenkins-ssh-keypair.pem ec2-user@54.245.238.194'
-
-alias bounceCommerceUi='curl --user admin:admin "http://localhost:8080/manager/text/reload?path=/commerceui"'
-alias github-fe='open https://github.com/bestbuyremix/commerce_anywhere_fe'
-alias github-rest='open https://github.com/bestbuyremix/commerce_ui'
-alias github-partner='open https://github.com/bestbuyremix/commerce_anywhere_partner'
-alias github-sim='open https://github.com/bestbuyremix/commerce_anywhere_sim'
 alias github-bully='open https://github.com/jmuraski/mean_playground_bully'
-alias jira='open "https://bbyopen.jira.com/secure/RapidBoard.jspa?rapidView=35"'
-alias jenkins='open "http://54.245.238.194/"'
+
+# Best Buy Aliases
+#alias ssh-ca-dev30='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-245-110-30.us-west-2.compute.amazonaws.com'
+#alias ssh-ca-dev-tomcat='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-245-110-30.us-west-2.compute.amazonaws.com'
+#alias ssh-ca-dev32='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-245-110-32.us-west-2.compute.amazonaws.com'
+#alias ssh-ca-dev-apache='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-245-110-32.us-west-2.compute.amazonaws.com'
+#alias ssh-ca-uat40='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-214-28-40.us-west-2.compute.amazonaws.com'
+#alias ssh-ca-uat-tomcat='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-214-28-40.us-west-2.compute.amazonaws.com'
+#alias ssh-ca-uat35='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-214-28-35.us-west-2.compute.amazonaws.com'
+#alias ssh-ca-uat-apache='ssh -v -i ~/.ssh/keys/ca-ssh-keypair.pem ec2-user@ec2-54-214-28-35.us-west-2.compute.amazonaws.com'
+#alias ssh-jenkins='ssh -v -i ~/.ssh/keys/jenkins-ssh-keypair.pem ec2-user@54.245.238.194'
+#alias bounceCommerceUi='curl --user admin:admin "http://localhost:8080/manager/text/reload?path=/commerceui"'
+#alias github-fe='open https://github.com/bestbuyremix/commerce_anywhere_fe'
+#alias github-rest='open https://github.com/bestbuyremix/commerce_ui'
+#alias github-partner='open https://github.com/bestbuyremix/commerce_anywhere_partner'
+#alias github-sim='open https://github.com/bestbuyremix/commerce_anywhere_sim'
+#alias jira='open "https://bbyopen.jira.com/secure/RapidBoard.jspa?rapidView=35"'
+#alias jenkins='open "http://54.245.238.194/"'
+#alias artifactory='open "https://code.bestbuy.com/artifactory/"'
+
+#Dev Jam Aliases
+alias ssh-devjam-ci='ssh root@192.241.248.176'
+alias ports-devjam="ssh root@107.170.22.200 docker ps | grep -o 'app.*' | sed -e 's/\(app\/[a-z,A-Z,_,0-9]*\).*\(0.0.0.0\)/\1 \2/' | awk '{print \$1 \" - \" \$2}'"
+alias github-tcfm='open https://github.com/DevJams/tcf_mobile'
+alias github-tcfs='open https://github.com/DevJams/tcf_simulator'
+alias jenkins-devjam='open "http://192.241.248.176:8090/"'
+
 alias sauce-connect="/usr/local/Sauce-Connect/open"
-alias artifactory='open "https://code.bestbuy.com/artifactory/"'
 
 alias chomeInsecure="open -a Google\ Chrome --args --disable-web-security"
 
