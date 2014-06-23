b20e707c6e2eeefeb6f86798fa44486d7b617c93
jmuraski <muraskijj@hotmail.com>
2014-06-23 11:00:44 -0500

    added some new variable mapping and modified path

--- lib/directories.zsh
+++ lib/directories.zsh
@@ -31,7 +31,8 @@ export HBASE_HOME="$HOMEBREW_HOME/hbase/0.94.2"
 export GRADLE_HOME="/usr/local/gradle-1.9"
 export GROOVY_HOME="/usr/local/groovy-2.2.1"
 export HOMEBREW_GITHUB_API_TOKEN="735a56b3d43f92cb78445c5fd9f7a76447df891e"
-export ANDROID_HOME="/Users/jmuraski/Documents/android/adt-bundle-mac-x86_64-20131030/sdk"
+export ANDROID_HOME="/Users/jmuraski/Documents/android/adt-bundle-mac-x86_64-20140321/sdk"
+export ANDROID_TOOLS="$ANDROID_HOME/platform-tools"
 export EDITOR='vim'
 export DOCKER_HOST=tcp://localhost:2375
 
